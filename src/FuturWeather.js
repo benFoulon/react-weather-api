0eb7fd5e5e01427956466b78233bb0987773f19c
BenFoulon <benjamin.foulon62@gmail.com>
2021-01-05 00:51:04 +0100

    bugfix

--- src/FuturWeather.js
+++ src/FuturWeather.js
@@ -25,26 +25,26 @@ export default class FuturWeather extends Component {
             return (
             <div className="next__day">
                 <div className="day">
-                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[1].weather[0].icon}.png`} alt=""/>
                     <h2 className="day__title">{new Date(daily[1].dt*1000).toLocaleString("fr-FR", {weekday:"long"})}</h2>
+                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[1].weather[0].icon}.png`} alt=""/>
                     <p>Température : {daily[1].temp.day} °C</p>
                     <p className="day__descrip">{daily[1].weather[0].description}</p>
                 </div>
                 <div className="day">
-                <img className="logo" src={`http://openweathermap.org/img/w/${daily[2].weather[0].icon}.png`} alt=""/>
                     <h2 className="day__title">{new Date(daily[2].dt*1000).toLocaleString("fr-FR", {weekday:"long"})}</h2>
+                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[2].weather[0].icon}.png`} alt=""/>
                     <p>Température : {daily[2].temp.day} °C</p>
                     <p className="day__descrip">{daily[2].weather[0].description}</p>
                 </div>
                 <div className="day">
-                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[3].weather[0].icon}.png`} alt=""/>
                     <h2 className="day__title">{new Date(daily[3].dt*1000).toLocaleString("fr-FR", {weekday:"long"})}</h2>
+                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[3].weather[0].icon}.png`} alt=""/>
                     <p>Température : {daily[3].temp.day} °C</p>
                     <p className="day__descrip">{daily[3].weather[0].description}</p>
                 </div>
                 <div className="day">
-                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[4].weather[0].icon}.png`} alt=""/>
                     <h2 className="day__title">{new Date(daily[4].dt*1000).toLocaleString("fr-FR", {weekday:"long"})}</h2>
+                    <img className="logo" src={`http://openweathermap.org/img/w/${daily[4].weather[0].icon}.png`} alt=""/>
                     <p>Température : {daily[4].temp.day} °C</p>
                     <p className="day__descrip">{daily[4].weather[0].description}</p>
                 </div>
